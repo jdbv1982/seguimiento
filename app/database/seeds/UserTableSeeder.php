7fc74111834566638054c84f37c3eb1d03ca3741
jdbv1982 <jdbardav@gmail.com>
2014-08-04 22:52:03 -0500

    Proyecto inicial de laravel incluye bootstrap

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+//use Faker\Factory as Faker;
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		User::create([
+			'full_name' => "Administrador General",
+			'email'	=> "jdbardav@gmail.com",
+			'password' => \Hash::make('admin'),
+			'available' => true
+		]);
+	}
+
+}
---
+++ app/views/layouts/layout.blade.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <link href="{{asset("assets/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset("assets/css/bootstrap.min.css") }}" rel="stylesheet">
+    
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+@yield('content')
+    
+<div class="container">
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="{{asset("assets/js/jquery.js")}}"></script>
+    <script src="{{asset("assets/bootstrap/js/bootstrap.min.js")}}"></script>
+  </body>
+</html>
